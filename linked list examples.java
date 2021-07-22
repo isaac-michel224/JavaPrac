7fdbbaa62846446f60090e5d01a483bbbedaf8e0
isaac-michel224 <84110054+isaac-michel224@users.noreply.github.com>
2021-07-22 08:34:55 -0400

    Add files via upload

---
+++ linked list examples.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+public class Class721 {
+    public static void main(String[] args) {
+        // {5, 4, 3, 6, 7}
+
+        // {value: 5 next: 1} -> {value: 8 next: 2} -> {value: 4 next: null}
+
+        // ArrayLists are faster at searching and sorting
+        // Linked Lists are faster at adding and removing data
+
+        LinkedList<Integer> linked = new LinkedList<>();
+        linked.add(5);
+        linked.add(7);
+        linked.add(3);
+        linked.addFirst(10);
+        System.out.println(linked.toString());
+        linked.add(1, 2);
+        System.out.println(linked.toString());
+        linked.set(4, 53);
+        System.out.println(linked.toString());
